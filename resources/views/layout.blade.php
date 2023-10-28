7dc7d8ea3fa507ff53668644012991cbdf40a27e
Tufi007 <taufeeqr346@gmail.com>
2023-10-28 21:37:20 +0530

    regular commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,9 +8,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/signup.css')}}">
+    <style>
+
+    </style>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>layoutpage</title>
 </head>
 
+<!------ Include the above in your HEAD tag ---------->
+
+<form>
+    <label>
+        <p class="label-txt">ENTER YOUR EMAIL</p>
+        <input type="text" class="input">
+        <div class="line-box">
+            <div class="line"></div>
+        </div>
+    </label>
+    <label>
+        <p class="label-txt">ENTER YOUR NAME</p>
+        <input type="text" class="input">
+        <div class="line-box">
+            <div class="line"></div>
+        </div>
+    </label>
+    <label>
+        <p class="label-txt">ENTER YOUR PASSWORD</p>
+        <input type="text" class="input">
+        <div class="line-box">
+            <div class="line"></div>
+        </div>
+    </label>
+    <button type="submit">submit</button>
+</form>
+<script src="{{asset('js/signup.js')}}"></script>
 <body>
 
 
