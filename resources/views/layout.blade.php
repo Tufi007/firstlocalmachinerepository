09eae5c0215891c13a3eb950b7d3a795efe28a88
Tufi007 <taufeeqr346@gmail.com>
2023-10-28 20:12:36 +0530

    regular commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,20 +12,8 @@
 </head>
 
 <body>
-    <h1>just the layout page</h1>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li>10</li>
-    <li>11</li>
-    <li>12</li>
-    
+
+
 </body>
 
 </html>
